60758f9165e9d85973290a3b761664857c6cc890
buzzefall <fxt3m4@gmail.com>
2018-09-06 19:46:33 +0700

    Areas added

---
+++ BookeNeest.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace BookeNeest.Web.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "Admin";
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                name: "Admin_Books",
+                url: "Admin/Books/{action}/{id}",
+                defaults: new {controller = "Books", action = "Recents", id = UrlParameter.Optional},
+                namespaces: new [] {"BookeNeest.Web.Areas.Admin.Controllers"}
+            );
+            
+            context.MapRoute(
+                name: "Admin_Default",
+                url: "Admin/{controller}/{action}/{id}",
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional},
+                namespaces: new [] {"BookeNeest.Web.Areas.Admin.Controllers", "BookeNeest.Web.Controllers"}
+            );
+        }
+    }
+}
---
+++ BookeNeest.Web/Areas/Admin/Controllers/BooksController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unity.Attributes;
+using AutoMapper;
+
+using BookeNeest.Domain.Contracts.Services;
+using BookeNeest.Web.Models;
+
+namespace BookeNeest.Web.Areas.Admin.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly IBookService bookService;
+
+        [InjectionConstructor]
+        public BooksController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+        // GET: Book
+        public ActionResult Recents()
+        {
+            // TODO: Autommaper dto -> view model - DONE
+            
+            // Dto
+            var books = bookService.GetRecentBooks(7);
+            var model = Mapper.Map<IList<BookViewModel>>(books);
+            
+            return View(model);
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(string bookName)
+        {
+            // TODO: Use services here?
+            //var container = new Unity.UnityContainer();
+            //var bookService = ;
+            var book = bookService.FindByName(bookName);
+
+            var model = Mapper.Map<BookViewModel>(book);
+
+            return View(model);
+        }
+
+        // GET: Book/Create
+        public ActionResult Add()
+        {
+            return View("AddNewBook");
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        public ActionResult Add(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Recents");
+            }
+            catch
+            {
+                return View("Recents");
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View("Recents");
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Recents");
+            }
+            catch
+            {
+                return View("Recents");
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View("Recents");
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Recents");
+            }
+            catch
+            {
+                return View("Recents");
+            }
+        }
+    }
+}
