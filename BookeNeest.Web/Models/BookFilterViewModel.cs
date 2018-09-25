eda327e307dbd44f2ea6333a951786bd35401745
Buzzefall <master.chief.cab@gmail.com>
2018-09-25 14:18:09 +0700

    Fixed SelectList

--- BookeNeest.Web/Models/BookFilterViewModel.cs
+++ BookeNeest.Web/Models/BookFilterViewModel.cs
@@ -6,12 +6,15 @@ using System.Web;
 
 namespace BookeNeest.Web.Models
 {
+    //if MinRating is not nullable, it is initialized with 0;
+    //hence SelectList takes that value as initial selected one and ignores selectedValue parameter of new SelectList() => useless constructor parameter;
+    //Statement: asp.net SelectList is shit.  
     public class BookFilterViewModel
     {
         public string Name { get; set; }
         public string Genres { get; set; }
         public string Authors { get; set; }
-        public int MinRating { get; set; }
-        public int MaxRating { get; set; }
+        public int? MinRating { get; set; }
+        public int? MaxRating { get; set; }
     }
 }
